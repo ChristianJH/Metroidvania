a0986d8949f557a5904a559a11ebedc569df563e
Christian Hall <cjh10@hampshire.edu>
2012-04-04 22:41:39 -0400

    Borders

--- TESTLEVEL/loadstuff.py
+++ TESTLEVEL/loadstuff.py
@@ -5,6 +5,10 @@ import pygame
 from pygame.locals import *
 from pygame.sprite import Sprite
 
+scale = 2
+
+SCREEN = 256*scale,224*scale
+
 def load_image(name, colorkey=None):
     path = os.path.join("data", "images", name) + ".bmp"
 
@@ -13,7 +17,6 @@ def load_image(name, colorkey=None):
         image.set_colorkey(colorkey)
     return image
 
-    
 
 class TileSheet(object):
     _map = {
@@ -71,11 +74,15 @@ class PlayerTiles(TileSheet):
     
 
 class Player(Sprite):
-    def __init__(self, tilesheet):
+    def __init__(self, tilesheet, facing):
         Sprite.__init__(self)
         self.tilesheet = tilesheet
-        self.image = self.tilesheet.get("face_left")
-        self.rect = self.image.get_rect()
+        self.facing = facing
+        if self.facing == "left":
+            self.image = self.tilesheet.get("face_left")
+        elif self.facing == "right":
+            self.image = self.tilesheet.get("face_right")        
+        self.rect = self.image.get_rect(center=(.5*SCREEN[0],.66*SCREEN[1]))
 
 
 class Level(object):
@@ -90,21 +97,39 @@ class Level(object):
 
 def main():
     pygame.init()
-    screen = pygame.display.set_mode((200,200))
+    screen = pygame.display.set_mode(SCREEN)
     pygame.key.set_repeat(50,50)
-    
+    facing = "right"
+    player_width,player_height = 30,38
     img_tiles = load_image("zelda2nestiles", (0,200,255))
     tilesheet = TileSheet(img_tiles, (16, 16))
     
     player_tiles = load_image("images", (255,255,255))
-    player_tilesheet = PlayerTiles(player_tiles, (30, 38))
+    player_tilesheet = PlayerTiles(player_tiles, (player_width, player_height))
 
     level = Level("level1", tilesheet)
-    player = Player(player_tilesheet)
+    player = Player(player_tilesheet,facing)
+    player_right = (screen.get_width()+player_width)/2
+    player_left = (screen.get_width()-player_width)/2
+    player_top = (screen.get_height()*.66-player_height/2)
+    player_bottom = (screen.get_height()*.66+player_height/2)
     off_x, off_y = 0,0
-    bound_x = level.image.get_width() - screen.get_width()
-    bound_y = level.image.get_height() - screen.get_height()
-
+    
+    
+    
+    # off_x, off_y = player.image.get_rect()  
+    """
+    bound_x = level.image.get_width() - (screen.get_width()-player_width)/2
+    """
+    
+    
+    
+    bound_x = level.image.get_width()
+        
+        
+        
+    bound_y = level.image.get_height()
+        
     done = False
     clock = pygame.time.Clock()
     while not done:
@@ -116,26 +141,38 @@ def main():
                 done = True
             elif event.type == KEYDOWN and event.key == K_LEFT:
                 off_x += 5
+                facing = "left"
             elif event.type == KEYDOWN and event.key == K_RIGHT:
                 off_x -= 5
+                facing = "right"
             elif event.type == KEYDOWN and event.key == K_UP:
                 off_y += 5
             elif event.type == KEYDOWN and event.key == K_DOWN:
                 off_y -= 5
         
-        if off_x > 0:
-            off_x = 0
-        elif off_x < -bound_x:
-            off_x = -bound_x
+        if off_x > player_left:
+            off_x = player_left
+        elif off_x < -bound_x+player_right:
+            off_x = -bound_x+player_right
+        if off_y > player_top:
+            off_y = player_top        
+        elif off_y < -bound_y + player_bottom:
+            off_y = -bound_y + player_bottom
+        """
         if off_y > 0:
             off_y = 0
         elif off_y < -bound_y:
             off_y = -bound_y
+
         print off_x, off_y, bound_x
-
-
+    
+        """
+        print off_x
+                
         screen.fill((80,80,80))
         screen.blit(level.image, (off_x,off_y))
+        player = Player(player_tilesheet, facing)
+               
         screen.blit(player.image, player.rect)
 
         pygame.display.flip()
